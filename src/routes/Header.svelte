b1383ffbb9731b1ecbf40df87ee9797c36b10bbf
Angel Martínez <angelmtzdiaz@gmail.com>
2024-10-22 18:04:13 -0600

    feat(): impl  rune + default props

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-    let { name = "human" } = $props()
+    let { name,
+        fakeName = null
+     }: {
+        name: string,
+        fakeName?: string | null
+     } = $props()
+    let storeName = $derived(fakeName || name + "'s Store" )
 </script>
 
-<h1>Hello ´{name}´ from Header</h1>
-<h2>{name.toUpperCase()}'s Store</h2>
+<h1>Hello ´{fakeName || name}´ from Header</h1>
+<h2>{storeName}</h2>
+
+{fakeName}
 
 <style>
 
