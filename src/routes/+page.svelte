fcc7d79d066439a1c54a05837f8694e64eb18194
Angel Martínez <angelmtzdiaz@gmail.com>
2024-10-25 03:02:33 -0600

    feat: impl animations + better code mgmt (snippets&loops)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,50 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition';
 	import Header from './Header.svelte';
 
 	type StrError = string;
-	const steps = {
-		NAME: 0,
-		BIRTHDAY: 1,
-		HOBBY: 2,
-		FINISHED: 3
-	};
+
 
 	let formState = $state({
 		name: '',
 		birthday: '',
 		hobby: '',
+		favoriteColor: '',
 		step: 0,
 		error: ''
 	});
+
+	const steps: {
+		key: keyof typeof formState;
+		question: string;
+		type: 'text' | 'date' | 'color';
+		validator: (arg: string) => StrError;
+	}[] = [
+		{
+			key: 'name',
+			question: 'Your Name',
+			type: 'text',
+			validator: validateName
+		},
+		{
+			key: 'birthday',
+			question: 'Your Birthday',
+			type: 'date',
+			validator: validateBirthday
+		},
+		{
+			key: 'hobby',
+			question: 'Your Hobby',
+			type: 'text',
+			validator: validateHobby
+		},
+		{
+			key: 'favoriteColor',
+			question: 'Your Favorite Color',
+			type: 'color',
+			validator: validateFavoriteColor
+		}
+	];
 
 	function validateName(name: string): StrError {
 		if (!name) return 'missing name';
@@ -44,10 +73,24 @@
 		return '';
 	}
 
+	function validateFavoriteColor(color: string): StrError {
+		if (!color) return 'missing favorite color';
+
+		return '';
+	}
+
 	function validateStep(): StrError {
-		if (formState.step === steps.NAME) return validateName(formState.name);
-		else if (formState.step === steps.BIRTHDAY) return validateBirthday(formState.birthday);
-		else if (formState.step === steps.HOBBY) return validateHobby(formState.hobby);
+		for (let i = 0; i < steps.length; i++) {
+			if (i === formState.step) {
+				const stepValue = formState[steps[i].key];
+				return steps[i].validator(stepValue as string);
+			}
+		}
+
+		// if (formState.step === steps.NAME) return validateName(formState.name);
+		// else if (formState.step === steps.BIRTHDAY) return validateBirthday(formState.birthday);
+		// else if (formState.step === steps.HOBBY) return validateHobby(formState.hobby);
+		// else if (formState.step === steps.) return validateHobby(formState.hobby);
 
 		return '';
 	}
@@ -76,35 +119,42 @@
 <main>
 	<p>step: {formState.step}</p>
 
-	{#if formState.step === steps.NAME}
-		<div>
-			{@render formStep({id: "name", type: "text", question: "Your Name"})}
-		</div>
+	{@render formItems({ include: ['name', 'birthday', 'hobby', "favoriteColor"] })}
+
+	{#each steps as { type, key, question }, i}
+		{#if formState.step === i}
+			{@render formStep({ type, id: key, question })}
+		{/if}
+	{/each}
+
+	<!-- {#if formState.step === steps.NAME}
+		{@render formStep({ id: 'name', type: 'text', question: 'Your Name' })}
 	{:else if formState.step === steps.BIRTHDAY}
-		<div>
-			{@render formItems({ include: ["name"] })}
-			{@render formStep({ id: "birthday", type: "date", question: "Your Birthday" })}
-		</div>
+		{@render formStep({ id: 'birthday', type: 'date', question: 'Your Birthday' })}
 	{:else if formState.step === steps.HOBBY}
-		<div>
-			{@render formItems({ include: ["name", "birthday"] })}
-			{@render formStep({ id: "hobby", type: "text", question: "Your Hobby" })}
-		</div>
-	{/if}
-	{#if formState.step === steps.FINISHED}
-		<div>
+		{@render formStep({ id: 'hobby', type: 'text', question: 'Your Hobby' })}
+	{/if} -->
+	<!-- {#if formState.step === steps.FINISHED}
+		<div in:fly={{
+			x: 200,
+			opacity: 0,
+			delay: 250
+		}} out:fly={{
+			x: -200,
+			opacity: 0
+		}}>
 			{@render formItems({ include: ["name", "birthday", "hobby"] })}
 		</div>
-	{/if}
+	{/if} -->
 
 	<span class="buttons">
-		{#if formState.step > steps.NAME}
+		{#if formState.step > 0}
 			<button onclick={previousStep}>Previous</button>
 		{/if}
-		{#if formState.step < steps.HOBBY}
+		{#if formState.step < steps.length - 1}
 			<button onclick={nextStep}>Next</button>
 		{/if}
-		{#if formState.step === steps.HOBBY}
+		{#if formState.step === steps.length - 1}
 			<button onclick={nextStep}>Finish</button>
 		{/if}
 	</span>
@@ -114,18 +164,32 @@
 	{/if}
 </main>
 
-{#snippet formStep({ id, type, question }: {
-	id: keyof typeof formState,
-	type: "text" | "date",
-	question: string
+{#snippet formStep({
+	id,
+	type,
+	question
+}: {
+	id: keyof typeof formState;
+	type: 'text' | 'date' | 'color';
+	question: string;
 })}
-	<label for={id} {id}>{question}</label>
-	<input {type} {id} bind:value={formState[id]}>
+	<div
+		in:fly={{
+			x: 200,
+			opacity: 0,
+			delay: 250
+		}}
+		out:fly={{
+			x: -200,
+			opacity: 0
+		}}
+	>
+		<label for={id} {id}>{question}</label>
+		<input {type} {id} bind:value={formState[id]} />
+	</div>
 {/snippet}
 
-{#snippet formItems({ include = [] }: {
-	include: (keyof typeof formState)[]
-})}
+{#snippet formItems({ include = [] }: { include: (keyof typeof formState)[] })}
 	{#each include as item}
 		<p>{item}: {formState[item]}</p>
 	{/each}
